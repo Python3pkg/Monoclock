4e9d84316290f68d2f071b4d1627ed90ea458ce4
Python3pkg <raliclo@gmail.com>
2017-05-21 09:56:52 -0700

    Convert to Python3

--- run_tests.py
+++ run_tests.py
@@ -25,9 +25,9 @@ def _parent(path):
 if __name__ == '__main__':
 	packageParentDir = _parent(os.path.abspath(__file__))
 	sys.path.insert(0, packageParentDir)
-	print "sys.path[0] is now", repr(sys.path[0])
-	print
-	print "Note that unittest swallows import-time exceptions.  If you see an"
-	print "exception below, it might be masking some other exception."
-	print "=" * 78
+	print("sys.path[0] is now", repr(sys.path[0]))
+	print()
+	print("Note that unittest swallows import-time exceptions.  If you see an")
+	print("exception below, it might be masking some other exception.")
+	print("=" * 78)
 	main()
--- test_monoclock.py
+++ test_monoclock.py
@@ -17,12 +17,12 @@ class MonoclockTests(unittest.TestCase):
 	def test_nano_count_isCorrectType(self):
 		val1 = monoclock.nano_count()
 		# In reality it is really always a long, I think.
-		self.assertTrue(isinstance(val1, (int, long)))
+		self.assertTrue(isinstance(val1, int))
 
 
 	def test_nano_count_getsBigger(self):
 		values = []
-		for n in xrange(10000):
+		for n in range(10000):
 			values.append(monoclock.nano_count())
 
 		# Values either get bigger or stay the same
